9ec59ae5465e245455f72e575e74bcd372601bc7
Jaysen Naidoo <jaysen@gmail.com>
2026-04-01 11:11:00 +0200

    data and src data folders in use. each with gitignored private folders

--- src/utility_scripts/find_clusters.js
+++ src/utility_scripts/find_clusters.js
@@ -21,7 +21,7 @@ const path = require('path');
 
 const CSV_PATH = path.join(
   __dirname,
-  '../docs/private/Map data FINAL 10Mar26.xlsx - final map data.csv'
+  '../data/private/Map data FINAL 10Mar26.csv'
 );
 
 const radiusDeg   = parseFloat(process.argv[2]) || 0.1;
