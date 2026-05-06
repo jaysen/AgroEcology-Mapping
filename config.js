4387465036adb10adfb159f82e30ad9396de962c
Jaysen Naidoo <jaysen@gmail.com>
2026-05-06 20:32:46 +0200

    Updates

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
 //   'https://docs.google.com/...'       — Google Sheets "publish to web" CSV URL
 //   'https://example.com/data.csv'      — any publicly accessible CSV URL
 window.AGROMAP_CONFIG = {
-  dataUrl: "./data.csv",
-  //dataUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS7_Ni--rxKqhKN8s1q73_9_ieLiN5IVFcrdAs24TtagQcK0Kht4yInMLHGCUp2FnBNelINiFo71-80/pub?gid=838223701&single=true&output=csv',
+  //dataUrl: "./data.csv",
+  dataUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS7_Ni--rxKqhKN8s1q73_9_ieLiN5IVFcrdAs24TtagQcK0Kht4yInMLHGCUp2FnBNelINiFo71-80/pub?gid=838223701&single=true&output=csv',
 };
